c058402ae70a885aad414c2571de143eed3ae18b
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-08-22 15:09:26 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,13 +14,3 @@ fido.bark
 snoopy = Dog.new
 snoopy.bark
 
-
-class Person 
-    def talk 
-        puts "Hello World!"
-    end
-
-    def walk 
-        puts "The Person is walking"
-    end
-end
